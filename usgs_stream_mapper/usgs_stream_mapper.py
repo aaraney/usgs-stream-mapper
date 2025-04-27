369d4e6b37515db7c7baf185c620156474670ef8
Austin Raney <aaraney@protonmail.com>
2025-04-27 13:37:47 -0400

    chore: add qt rc file and update all related file references

--- usgs_stream_mapper/usgs_stream_mapper.py
+++ usgs_stream_mapper/usgs_stream_mapper.py
@@ -171,7 +171,7 @@ class UsgsStreamMapper:
     def initGui(self):
         """Create the menu entries and toolbar icons inside the QGIS GUI."""
 
-        icon_path = ':/plugins/usgs_stream_mapper/resources/icon.png'
+        icon_path = ':/plugins/usgs_stream_mapper/icon.png'
         self.add_action(
             icon_path,
             text=self.tr(u'Usgs Stream Mapper'),
